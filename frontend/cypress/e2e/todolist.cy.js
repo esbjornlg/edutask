dee3d1fef6ee9c05051ab779cd59b6356a64ef0c
esbjornlg <esbjornlystergolawski@gmail.com>
2023-04-28 19:02:32 +0200

    Assignment4: R8UC1 and Setup works

--- frontend/cypress/e2e/todolist.cy.js
+++ frontend/cypress/e2e/todolist.cy.js
@@ -2,7 +2,7 @@ describe("R8UC1", () => {
   let email = "Namn@gmail.com";
   let firstName = "Namn";
   let lastName = "Namnsson";
-  before(function () {
+  beforeEach(function () {
     cy.visit("localhost:3000");
     cy.contains("div", "Email Address").find("input[type=text]").type(email);
     cy.get("input[value=Login]").click();
@@ -23,51 +23,55 @@ describe("R8UC1", () => {
       cy.contains("div", "Last Name").find("input[type=text]").type(lastName);
       cy.get("input[value='Sign Up']").click();
     }
-
-    let task;
+    
     let taskName = "Pawn Stars";
     let youtubeURL = "DRVlUDQCmNk";
-    //try to find existing task
-    // cy.$$("a:contains('img')").then($task => {
-    //   task = $task;
-    // })
-    cy.contains("div", "Title").find("input[type=text]").type(taskName);
-    cy.get("input#url").type(youtubeURL);
-    cy.get('input[type="submit"][value="Create new Task"]').click();
-    cy.get('a:has(img[src="http://i3.ytimg.com/vi/DRVlUDQCmNk/hqdefault.jpg"])')
-      .first()
-      .click();
-  });
+    cy.contains("div", "Title")
+      .find("input[type=text]")
+      .type(taskName);
+    cy.contains("div", "YouTube URL")
+      .find("input[type=text]")
+      .type(youtubeURL);
+    cy.get(".container").then($container => {
+      if ($container["0"].children.length > 1) {
+        cy.get('a:has(img)')
+        .first()
+        .click();
+      } else {
+        cy.get('input[type="submit"][value="Create new Task"]').click();
+      }
+    })
+  })
 
-  it("Find input, type text, and click submit", () => {
-    // Replace 'Your sample text here' with the text you want to type
-    const textToType = "Sample text here";
 
-    // Find the input element by its placeholder and type the text
+  it('Add new todo item with valid text', () => {
+    cy.viewport(1000, 1000);
+    let textToType = "Watch video";
+
     cy.get('input[placeholder="Add a new todo item"]').type(textToType);
 
-    // Click the submit button if it's enabled
-    cy.get('input[type="submit"][value="Add"]')
-      .then(($submitButton) => {
-        if (!$submitButton.attr("disabled")) {
-          cy.wrap($submitButton).click();
-        }
-      })
+    cy.get('input[type="submit"][value="Add"]').click()
       .then(() => {
         cy.contains("span.editable", textToType).should("exist");
       });
   });
+
+it('Add new todo item with invalid text', () => {
+  cy.viewport(1000, 1000);
+  cy.get('[value="Add"]').should('have.attr', 'disabled');
 });
-describe("R8UC2 check", () => {
-  it("Checking if button icon is set to active ", () => {
-    cy.get(".checker.unchecked").click();
-    cy.get(".editable").should(
-      "have.css",
-      "text-decoration",
-      "line-through solid rgb(0, 0, 0)"
-    );
-  });
-});
+
+}); 
+// describe("R8UC2 check", () => {
+//   it("Checking if button icon is set to active ", () => {
+//     cy.get(".checker.unchecked").click();
+//     cy.get(".editable").should(
+//       "have.css",
+//       "text-decoration",
+//       "line-through solid rgb(0, 0, 0)"
+//     );
+//   });
+// });
 
 // describe('R8UC2 uncheck', () => {
 
