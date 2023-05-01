ed857e3738e9341836c0208c52a09c1718253e43
JamesTTTT <taylormarkaryd@gmail.com>
2023-05-01 15:06:11 +0200

    Updated R8UC2 and setup to only create one task

--- frontend/cypress/e2e/todolist.cy.js
+++ frontend/cypress/e2e/todolist.cy.js
@@ -26,76 +26,65 @@ describe("R8UC1", () => {
 
     let taskName = "Pawn Stars";
     let youtubeURL = "DRVlUDQCmNk";
-    cy.contains("div", "Title")
-      .find("input[type=text]")
-      .type(taskName);
-    cy.contains("div", "YouTube URL")
-      .find("input[type=text]")
-      .type(youtubeURL);
-    cy.get(".container").then($container => {
-      if ($container["0"].children.length > 1) {
-        cy.get('a:has(img)')
-          .first()
-          .click();
+    cy.contains("div", "Title").find("input[type=text]").type(taskName);
+    cy.contains("div", "YouTube URL").find("input[type=text]").type(youtubeURL);
+    cy.get(".container").then(($container) => {
+      if ($container["0"].children.length > 0) {
+        cy.get(".container-element:first").first().click();
       } else {
         cy.get('input[type="submit"][value="Create new Task"]').click();
       }
-    })
-  })
+    });
+  });
 
-
-  it('R8UC1_1: Add new todo item with valid text', () => {
+  it("R8UC1_1: Add new todo item with valid text", () => {
     cy.viewport(1000, 1000);
     let number = Math.floor(Math.random() * 10000);
     let textToType = "Shop at pawnstar" + number;
     addTodoItem(textToType);
   });
 
-  it('R8UC1_2: Add new todo item with invalid text', () => {
+  it("R8UC1_2: Add new todo item with invalid text", () => {
     cy.viewport(1000, 1000);
-    cy.get('[value="Add"]').should('have.attr', 'disabled');
+    cy.get('[value="Add"]').should("have.attr", "disabled");
   });
 
-  it('R8UC3_1: Add and then remove a todo item', () => {
+  it("R8UC2_1: check subtask", () => {
+    cy.viewport(1000, 1000);
+
+    cy.get(".checker.unchecked:first").should("be.visible").click();
+    cy.wait(500);
+    cy.get(".checker.checked:first + .editable")
+      .should("have.css", "text-decoration")
+      .and("contain", "line-through");
+  });
+
+  it("R8UC2_2: uncheck subtask", () => {
+    cy.viewport(1000, 1000);
+    cy.get(".checker.checked:first").should("exist").and("be.visible");
+    cy.get(".checker.checked:first").click();
+    cy.wait(500);
+    cy.get(".checker.unchecked:first + .editable")
+      .should("have.css", "text-decoration")
+      .and("not.contain", "line-through");
+  });
+
+  it("R8UC3_1: Add and then remove a todo item", () => {
     cy.viewport(1000, 1000);
     let number = Math.floor(Math.random() * 10000);
     let textToType = "RemoveThisItem" + number;
     addTodoItem(textToType);
-    cy.contains('li', textToType)
-      .find('span').
-      contains('✖')
-      .click();
+    cy.contains("li", textToType).find("span").contains("✖").click();
     cy.contains("span.editable", textToType).should("not.exist");
   });
-
 });
 
 function addTodoItem(todoName) {
   cy.get('input[placeholder="Add a new todo item"]').type(todoName);
 
-  cy.get('input[type="submit"][value="Add"]').click()
+  cy.get('input[type="submit"][value="Add"]')
+    .click()
     .then(() => {
       cy.contains("span.editable", todoName).should("exist");
     });
 }
-
-
-
-// describe("R8UC2 check", () => {
-//   it("Checking if button icon is set to active ", () => {
-//     cy.get(".checker.unchecked").click();
-//     cy.get(".editable").should(
-//       "have.css",
-//       "text-decoration",
-//       "line-through solid rgb(0, 0, 0)"
-//     );
-//   });
-// });
-
-// describe('R8UC2 uncheck', () => {
-
-//   it('Checking if button icon is set to active ', () => {
-//     cy.visit('https://localhost:3000')
-//     cy.get('.checker.checked').click()
-//   })
-// })
